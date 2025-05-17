7772e2732a920d106d0b99d4cbabe4997e3d18d9
hugob <hgbequet@gmail.com>
2025-05-17 19:19:09 +0200

    Delete of useless comment in config.hpp

--- src/config.hpp
+++ src/config.hpp
@@ -1,15 +1,9 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
-#include <string>
-#include <unordered_map>
-#include <instructions/Instruction.hpp>
 
 enum class ValueType {
     IMMEDIATE_VALUE,
     REGISTER
 };
-
-//std::unordered_map<std::string, Instruction> instructions_look_map;
-
 
 #endif //CONFIG_HPP
